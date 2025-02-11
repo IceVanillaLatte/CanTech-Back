1d05c388652ed8d8581d4f7a27f86bd54e9542b3
yujinKim <yujinkimvv@gmail.com>
2025-02-11 17:15:06 +0900

    Fix: 오타 수정 (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.8"
 	id("io.spring.dependency-management") version "1.1.7"
-	id "org.sonarqube" version "6.0.1.5171"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "com.cantech.backend"
@@ -15,10 +15,10 @@ java {
 }
 
 sonar {
-	properties {
-		property "sonar.projectKey", "backend-service"
-		property "sonar.projectName", "backend-service"
-	}
+  properties {
+    property("sonar.projectKey", "backend-service")
+    property("sonar.projectName", "backend-service")
+  }
 }
 
 repositories {
